3f1e378ae8e108f9b81b0ea9faf992d757865ef8
Dynarithmic <paulm@dynarithmic.com>
2025-06-29 21:58:46 -0400

    Update Visual Basic demo

---
+++ FullDemo/SourcePropertiesDlg.vb
@@ -0,0 +1,79 @@
+﻿Imports System.Text
+
+
+Public Class SourcePropertiesDlg
+
+    Private m_Source As System.IntPtr
+
+    Public Sub New(ByVal item As System.IntPtr)
+        InitializeComponent() ' This call is required by the Windows Form Designer.
+        m_Source = item
+    End Sub
+    Private Sub OK_Button_Click(ByVal sender As System.Object, ByVal e As System.EventArgs)
+        Me.DialogResult = System.Windows.Forms.DialogResult.OK
+        Me.Close()
+    End Sub
+
+    Private Sub Cancel_Button_Click(ByVal sender As System.Object, ByVal e As System.EventArgs)
+        Me.DialogResult = System.Windows.Forms.DialogResult.Cancel
+        Me.Close()
+    End Sub
+
+    Private Sub SourcePropertiesDlg_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        Dim szInfo As New System.Text.StringBuilder(256)
+        Dim szInfoName As New System.Text.StringBuilder(256)
+        DTWAINAPI.DTWAIN_GetSourceProductName(m_Source, szInfoName, 255)
+        Me.edProductName.Text = szInfoName.ToString()
+        DTWAINAPI.DTWAIN_GetSourceProductFamily(m_Source, szInfo, 255)
+        Me.edFamilyName.Text = szInfo.ToString()
+        DTWAINAPI.DTWAIN_GetSourceManufacturer(m_Source, szInfo, 255)
+        Me.edManufacturer.Text = szInfo.ToString()
+        DTWAINAPI.DTWAIN_GetSourceVersionInfo(m_Source, szInfo, 255)
+        Me.edVersionInfo.Text = szInfo.ToString()
+
+        Dim lMajor As Integer
+        Dim lMinor As Integer
+        lMajor = 0
+        lMinor = 0
+        DTWAINAPI.DTWAIN_GetSourceVersionNumber(m_Source, lMajor, lMinor)
+        Dim sVersion As String
+        sVersion = lMajor.ToString() + "." + lMinor.ToString()
+        Me.edVersion.Text = sVersion
+
+        Dim AllCaps As System.IntPtr
+        Dim Val As Integer
+        DTWAINAPI.DTWAIN_EnumSupportedCaps(m_Source, AllCaps)
+        Dim nSize As Integer
+        nSize = DTWAINAPI.DTWAIN_ArrayGetCount(AllCaps)
+        For i As Integer = 0 To nSize - 1
+            DTWAINAPI.DTWAIN_ArrayGetAtLong(AllCaps, i, Val)
+            DTWAINAPI.DTWAIN_GetNameFromCap(Val, szInfo, 255)
+            Me.listCaps.Items.Add(szInfo.ToString())
+        Next i
+
+        Me.edTotalCaps.Text = nSize.ToString()
+        DTWAINAPI.DTWAIN_EnumCustomCaps(m_Source, AllCaps)
+        Me.edCustomCaps.Text = DTWAINAPI.DTWAIN_ArrayGetCount(AllCaps).ToString()
+        DTWAINAPI.DTWAIN_EnumExtendedCaps(m_Source, AllCaps)
+        Me.edExtendedCaps.Text = DTWAINAPI.DTWAIN_ArrayGetCount(AllCaps).ToString()
+
+        Dim customDSLength As UInteger
+        Dim jsonLength As Integer
+        Dim enc8 As Encoding = Encoding.UTF8
+        DTWAINAPI.DTWAIN_GetCustomDSData(m_Source, IntPtr.Zero, 0, customDSLength, DTWAINAPI.DTWAINGCD_COPYDATA)
+        Dim szCustomData(customDSLength) As Byte
+        DTWAINAPI.DTWAIN_GetCustomDSData(m_Source, szCustomData, customDSLength, customDSLength, DTWAINAPI.DTWAINGCD_COPYDATA)
+        Dim contents As String
+        contents = enc8.GetString(szCustomData, 0, customDSLength)
+        Me.txtDSData.Text = contents
+        Dim sName As String
+        sName = szInfoName.ToString()
+        jsonLength = DTWAINAPI.DTWAIN_GetSourceDetails(sName, IntPtr.Zero, 0, 2, 1)
+        szInfo = New StringBuilder(jsonLength)
+        DTWAINAPI.DTWAIN_GetSourceDetails(sName, szInfo, jsonLength, 2, 1)
+
+        ' Convert string to one with /r/n, since these are the types of strings for edit controls
+        Dim sNewData As String = szInfo.ToString().Replace(vbLf, vbCrLf)
+        Me.txtJSON.Text = sNewData
+    End Sub
+End Class
---
+++ README.md
@@ -0,0 +1,6 @@
+# twain_library-visualbasic_demo
+Visual Basic .NET demo program using the <a href="https://github.com/dynarithmic/twain_library" target="_blank">DTWAIN Library</a>.
+
+This demo consists of a Visual Studio solution, **VB_FullDemo.sln**.  To build the demo, simply load the solution into Visual Studio (at least VS 2019), and choose **Build -> Build Solution** from the Visual Studio main menu.
+
+Once built, the demo assumes that <a href="https://github.com/dynarithmic/twain_library/tree/master/binaries/32bit" target="_blank">dtwain32u.dll</a> is available (either on the system PATH or in the same directory as the executable) when the demo is run.  
